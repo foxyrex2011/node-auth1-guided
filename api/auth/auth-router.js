599bd0d4d303b883865b92243eb52a9dc3a81287
Frederick Cortez <frederickcortez66@gmail.com>
2023-05-02 08:37:59 -0500

    finished guided project

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,16 +1,48 @@
-const express =require ('express')
+const express = require ('express')
 const bcrypt = require('bcryptjs')
 const router = express.Router()
+const User = require('../users/users-model')
 
 router.post('/register', async (req, res, next) => {
-    const { username, password } = req.body
-    console.log( username, password)
+    try {
+        const { username, password } = req.body
+        const hash = bcrypt.hashSync(password, 8)
+        const newUser = { username, password: hash}
+        const result = await User.add(newUser)
+        res.status(201).json({
+            message: `nice to have you ${result.username}`,
+        })
+    } catch (err) {
+        next(err)
+    }
 })
 router.post('/login', async (req, res, next) => {
-    res.json({ message: 'login working'})
+    try {
+        const { username, password } = req.body
+        const [user] = await User.findBy({ username })
+        if (user && bcrypt.compareSync(password, user.password)) {
+            req.session.user = user
+            res.json({ message: `welcome back, ${user.username}` })
+        } else {
+            next ({status: 401, message: 'bad credentials'})
+        }
+    } catch (err){
+        next(err)
+    }
 })
 router.get('/logout', async (req, res, next) => {
-    res.json({ message: 'logout working'})
+    if (req.session.user) {
+        const { username } = req.session.user
+        req.session.destroy(err => {
+            if (err) {
+                res.json({ message: `you can never leave ${username}`})
+            } else {
+                res.json({ message: `Goodbye ${username}`})
+            }
+        })
+    } else {
+        res.json({ message: 'Sorry, have we met?'})
+    }
 })
 
 module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,5 +1,7 @@
 const path = require('path')
 const express = require('express')
+const session = require('express-session')
+const Store = require('connect-session-knex')(session)
 const authRouter = require('./auth/auth-router')
 const usersRouter = require('./users/users-router.js')
 
@@ -7,6 +9,25 @@ const server = express()
 
 server.use(express.static(path.join(__dirname, '../client')))
 server.use(express.json())
+server.use(session({
+  name: 'monkey',
+  secret: 'keep it secret',
+  cookie: {
+    maxAge: 100 * 60 * 60,
+    secure: false,
+    httpOnly: false,
+  },
+  rolling: true,
+  resave: false,
+  saveUninitialized: false,
+  store: new Store({
+    knex: require('../database/db-config'),
+    tablename: 'sessions',
+    sidfeildname: 'sid',
+    createtable: true,
+    clearInterval: 1000 * 60 * 60,
+  })
+}))
 server.use('/api/auth', authRouter)
 server.use('/api/users', usersRouter)
 
