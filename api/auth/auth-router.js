b192911655265380c86e0a97db86565df84dcd17
Frederick Cortez <frederickcortez66@gmail.com>
2023-05-02 06:33:26 -0500

    flushed post endpoint

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,14 +1,15 @@
 const express =require ('express')
-
+const bcrypt = require('bcryptjs')
 const router = express.Router()
 
-router.post('/register', async (req, res ,next) => {
-    res.json({ message: 'register working'})
+router.post('/register', async (req, res, next) => {
+    const { username, password } = req.body
+    console.log( username, password)
 })
-router.post('/login', async (req, res ,next) => {
+router.post('/login', async (req, res, next) => {
     res.json({ message: 'login working'})
 })
-router.get('/logout', async (req, res ,next) => {
+router.get('/logout', async (req, res, next) => {
     res.json({ message: 'logout working'})
 })
 
